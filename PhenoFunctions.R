c86712946e506a64e389aef6099f42463927cd9d
audhalbritter <me@audhalbritter.com>
2016-10-12 15:06:54 +0200

    initial commit

---
+++ PhenoFunctions.R
@@ -0,0 +1,57 @@
+# PhenoFunctions
+#### READ IN PHENOLOGY DATA 2015 ####
+ReadInBodyPhenology <- function(datasheet, site){
+  # import body of data
+  dat <- read.csv(datasheet, header=FALSE, sep=";", skip=3, stringsAsFactors=FALSE)
+  dat <- dat[dat$V2!="",] # get rid of empty lines, where no species
+  dat <- dat[,-3] # get rid of chinese names
+  dat$V2<-gsub("*", "", dat$V2,fixed = TRUE) # get rid of * and space
+  dat$V2<-gsub(" ", "", dat$V2,fixed = TRUE)
+  
+  # loop to get turfID in all cells
+  for (i in 2:nrow(dat)){
+  if(nchar(dat$V1[i])==0){
+      dat$V1[i] <- dat$V1[i-1]
+    }
+  }
+  # import head of data set
+  dat.h <- read.csv(datasheet, sep=";", header=FALSE, nrow=3, stringsAsFactors=FALSE)
+  
+  # merge data into long data table
+  long.table <- lapply(seq(3,ncol(dat)-15,16),function(i){
+    x <- dat[ ,c(1:2,i:(i+15))]
+    names(x) <- c("turfID", "species", paste(rep(c("b", "f", "s", "r"), 4  ), rep(1:4, each=4), sep="."))
+    x$date <- strsplit(dat.h[1,i+1], "_")[[1]][1]
+    x$doy <- yday(ymd(x$date))
+    x  
+  })
+  dat.long <- do.call(rbind,c(long.table, stingsAsFactors=FALSE))
+  
+  # Extract site
+  dat.long$origSite <- substr(dat.long$turfID, 1,1)
+  dat.long$destSite <- site
+  dat.long$block <- substr(dat.long$turfID, 2,2)
+  dat.long$treatment <- substr(dat.long$turfID, 4,nchar(dat.long$turfID))
+  
+  # convert to factor and numeric
+  dat.long <- cbind(dat.long[,c(1:2,19:24)],sapply(dat.long[,c(3:18)],as.numeric))
+  #sapply(dat.long[,c(4:7,9:12,14:17,19:22)],function(x)print(grep("\\D", x = x, value = TRUE))) # Check error messages
+  dat.long <- dat.long[-1,]
+  dat.long
+  return(dat.long)
+}
+
+
+
+# Calculate the sum of buds, flowers and seeds per turf and species
+CalcSums <- function(dat){
+  dat$nr.b <- apply(dat[,c(seq(9,21,4))],1,sum, na.rm=TRUE)
+  dat$nr.b[dat$nr.b == 0] <- NA
+  dat$nr.f <- apply(dat[,c(seq(10,22,4))],1,sum, na.rm=TRUE)
+  dat$nr.f[dat$nr.f == 0] <- NA
+  dat$nr.s <- apply(dat[,c(seq(11,23,4))],1,sum, na.rm=TRUE)
+  dat$nr.s[dat$nr.s == 0] <- NA
+  dat$nr.r <- apply(dat[,c(seq(12,24,4))],1,sum, na.rm=TRUE)
+  dat$nr.r[dat$nr.r == 0] <- NA
+  return(dat)
+}
