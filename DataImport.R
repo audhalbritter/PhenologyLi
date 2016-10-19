6d598f944cda40578d2e43bad2ee7dad0bb685d6
audhalbritter <me@audhalbritter.com>
2016-10-19 11:06:33 +0200

    rename variables

--- DataImport.R
+++ DataImport.R
@@ -55,23 +55,65 @@ pheno.long <- pheno %>%
   filter(first > minDoy) %>% # remove if plant is flowering in the first week
   ungroup() %>% 
   select(-minDoy) %>% # remove this variable
-  mutate_each(funs(as.numeric), first, peak, end) %>% # make variables numeric (probably not necessary)
+  #mutate_each(funs(as.numeric), first, peak, end) %>% # make variables numeric (probably not necessary)
+  # make the data nice, rename variables and order them
   mutate(pheno.stage = substring(pheno.stage, nchar(pheno.stage), nchar(pheno.stage))) %>%  # take last letter from pheno.stage
+  mutate(pheno.stage = factor(pheno.stage, levels = c("b", "f", "s", "r"))) %>% 
+  mutate(pheno.var = factor(pheno.var, levles =c("first", "peak", "end"))) %>%
   gather(key = pheno.var, value = value, -turfID, -species, -pheno.stage) %>%  # create pheno.var and gather 4 variable into 1 column
   mutate(treatment = substring(turfID, 4, nchar(turfID))) %>% 
   mutate(block = substring(turfID, 2,2)) %>% 
   mutate(originSite = substring(turfID,1,1))
 head(pheno.long)
 
+# add destSite
 pheno.long$destSite <- pheno.dat[match(pheno.long$turfID,pheno.dat$turfID),c("destSite")]
 
+mutate(newTT = plyr::mapvalues(newTT, c("control", "TT2", "TT3", "TT4"), c("Control", "Warm", "Wet", "WarmWet"))) %>%
+  mutate(newTT = factor(newTT, levels = c("Control", "Warm", "Wet", "WarmWet"))) %>%
+
+
+
+#### CALCULATE DAYS BETWEEN PHENO.STAGES IN DAYS ####
+### DURATION BETWEEN FIRST AND END OF PHENO.STAGES
+pheno.long <- pheno.long %>% 
+  select(turfID, species, newTT, value, pheno.var, pheno.stage, d.dosm, o.dosm) %>%
+  spread(key = pheno.stage, value = value) %>% 
+  # in days
+  mutate(d.smb = b - d.dosm) %>% 
+  mutate(o.smb = b - ifelse(newTT == "control", o.dosm, d.dosm)) %>% 
+  mutate(bf = f-b, fs = s-f) %>% # calculate difference in days between bud-flower and flower-seed
+  gather(key = pheno.stage, value = value, b, f, r, s, d.smb, o.smb, bf, fs) %>% 
+  mutate(pheno.unit = ifelse(pheno.stage %in% c("d.smb", "o.smb", "bf", "fs"), "days", "doy")) %>% # create variable pheno.unit
+  # calculate duration of stages
+  spread(key = pheno.var, value = value) %>% 
+  mutate(duration = ifelse(pheno.unit == "doy", end-(first-1), NA)) %>% # calculate duration
+  gather(key = pheno.var, value = value, end, first, peak, duration) %>% 
+  mutate(pheno.unit = replace(pheno.unit, pheno.var == "duration", "days")) %>% 
+  filter(!is.na(value))
+
+mutate(treatment = plyr::mapvalues(treatment, c("OTC", "C", "O", "1", "2"), c("OTC", "Control", "Local", "Warm", "Cold"))) %>% 
+  mutate(treatment = factor(treatment, levels=c("Control", "OTC", "Warm", "Cold", "Local"))) %>%
+  mutate(destSite = factor(pheno.var, levles =c("H", "A", "M"))) %>% 
+  mutate(originSite = factor(pheno.var, levles =c("H", "A", "M"))) %>% 
 
 # Making Figures
 pheno.long %>% 
-  filter(pheno.stage == "f", species == "All.cya") %>% 
-  group_by(species, pheno.var, treatment) %>% 
-  ggplot(aes(x = treatment, y = value)) +
-  geom_boxplot() +
-  facet_wrap(pheno.var~originSite)
+  #filter(pheno.stage == "f", species == "All.cya") %>% 
+  group_by(pheno.stage, pheno.var, treatment, destSite) %>% 
+  summarise(mean = mean(value)) %>% 
+  ggplot(aes(x = treatment, y = mean, color = pheno.var)) +
+  geom_point() +
+  facet_grid(pheno.stage~destSite)
+
+pheno %>% 
+  select(turfID, species, date, doy, origSite, destSite, block, treatment, nr.b, nr.f, nr.s, nr.r) %>%
+  filter(turfID == "A6-1", species == "Pot.leu") %>% 
+  gather(key = pheno.stage, value = value, nr.b, nr.f, nr.s, nr.r) %>%
+  filter(value > 0) %>%
+  ggplot(aes(x = doy, y = value, color = pheno.stage)) +
+  geom_line() +
+  facet_wrap(~ species, scales = "free")
+
 
 
