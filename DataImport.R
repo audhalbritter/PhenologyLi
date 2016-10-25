9a6f9a75d3d726ac8bb6abc34271f7113029e6ff
audhalbritter <me@audhalbritter.com>
2016-10-25 15:24:00 +0200

    Figure for all species comparing treatments

--- DataImport.R
+++ DataImport.R
@@ -110,20 +110,43 @@ setdiff(trait$sp, pheno.long$species)
 pheno.long <- pheno.long %>% left_join(trait, by = c("species" = "sp"))
 
 
-# Making Figures
+# Making Figures for each species
 pheno.long %>% 
-  #filter(treatment %in% c("OTC", "Control","Warm","Local")) %>% 
- # filter(functionalGroup == "forb") %>% 
+  filter(treatment %in% c("OTC", "Control")) %>% 
+  filter(origSite == "H") %>% 
+  #filter(functionalGroup == "forb") %>% 
   #filter(flTime == "early") %>%
-  filter(pheno.stage %in% c("b", "f", "s"), pheno.var != "duration") %>% 
+  filter(pheno.stage %in% c("f"), pheno.var %in% c("first", "duration")) %>% 
   #filter(pheno.stage %in% c("bf", "fs", "sr")) %>% 
   #filter(pheno.var %in% "duration") %>% 
-  group_by(pheno.stage, pheno.var, treatment, origSite) %>% 
-  #summarise(mean = mean(value)) %>% 
-  ggplot(aes(x = treatment, y = value, color=functionalGroup ) +
-  geom_boxplot() +
-  facet_grid( ~pheno.stage )
+  group_by(species, treatment, pheno.var) %>% 
+  summarise(mean = mean(value)) %>% 
+  spread(key = treatment, value = mean) %>% 
+  na.omit() %>% 
+  gather(key = treatment, value = value, -species, -pheno.var) %>% 
+  spread(key = pheno.var, value = value) %>% 
+  ggplot(aes(x = first, y = species, color = treatment)) + geom_point() +
+  geom_segment(aes(x=first, xend=(first+duration), y=species, yend=species),size=1)
 
+
+
+PhenologicalStages <- pheno.long %>% 
+    filter(treatment %in% c("OTC", "Control","Warm","Local")) %>% 
+    filter(origSite %in% c("H", "A")) %>% 
+    filter(functionalGroup %in% c("forb", "graminoid")) %>% 
+    #filter(flTime == c("early", "late")) %>%
+    #filter(pheno.stage %in% c("b", "f", "s"), pheno.var == "first") %>% 
+    #filter(pheno.stage %in% c("bf", "fs", "sr")) %>% 
+    filter(pheno.var %in% "duration") %>% 
+    #group_by(species, treatment) %>% 
+    #summarise(mean = mean(value)) %>% 
+    ggplot(aes(x = treatment, y = value, color = functionalGroup)) +
+    geom_boxplot() +
+    facet_grid(origSite~pheno.stage)
+  
+PhenologicalStages + theme_grey(base_size = 20) + theme(legend.title=element_blank()) + ggtitle("DOY")
+PhenologicalDuration + theme_grey(base_size = 20) + theme(legend.title=element_blank()) + ggtitle("Days")
+PhenologicalFirstEnd + theme_grey(base_size = 20) + theme(legend.title=element_blank()) + ggtitle("Days")
 
 
 # Climate data
