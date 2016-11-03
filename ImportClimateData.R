795aa0a52243c6809166f1748e4ccc022d567571
audhalbritter <me@audhalbritter.com>
2016-11-03 09:57:24 +0100

    Test regression daily temp at A and H site

--- ImportClimateData.R
+++ ImportClimateData.R
@@ -4,28 +4,69 @@ load("clean_weather.Rdata", verbose = TRUE)
 
 head(distinct_weather)
 
-ggplot(distinct_weather, aes(x = dateTime, y = Tsoil20, color = site)) +
+# plot the data
+ggplot(distinct_weather, aes(x = dateTime, y = Tair, color = site)) +
   geom_line() +
   facet_wrap(~site)
 
+
+#### CHECK REGRESSION BETWEEN TAIR IN A AND H SITE
+### Regression for 10 min data
+HAsite <- distinct_weather %>% 
+  select(site, dateTime, Tair) %>% 
+  filter(site %in% c("H", "A")) %>% 
+  spread(key = site, value = Tair)
+
+fit10 <- lm(H ~ A, data = HAsite)
+summary(fit10)
+plot(HAsite$A, HAsite$H)
+abline(fit10)
+
+
+### Regression for daily data
 ### Calculate Daily Temperature
-climate <- distinct_weather %>% 
-  filter(!is.na(dateTime) & !is.na(Tair) & !is.na(Tsoil20)) %>% # remove NA
-  select(site, dateTime, Tair, Tsoil20) %>% 
-  gather(key = "variable", value = "value", -site, -dateTime) %>% 
+dailyTemperature <- distinct_weather %>% 
+  select(site, dateTime, Tair) %>% 
+  filter(!is.na(dateTime)) %>% # remove NA in dateTime (2 rows)
   #mutate(dateHour = paste(format(dateTime, "%Y-%m-%d"), hour(dateTime), sep = "_")) %>% # for hourly data
-  mutate(dateDaily = format(dateTime, "%Y-%m-%d")) %>%
-  group_by(site, dateDaily, variable) %>% 
-  summarise(n = n(), mean = mean(value))
+  mutate(dateDaily = ymd(format(dateTime, "%Y-%m-%d"))) %>%
+  group_by(site, dateDaily) %>% 
+  summarise(n = n(), Tair = mean(Tair)) %>% 
+  filter(!n < 144)
+
+HAdaily <- dailyTemperature %>% 
+  filter(site %in% c("H", "A")) %>% 
+  spread(key = site, value = Tair) %>% 
+  na.omit()
+
+plot(HAdaily$A, HAdaily$H)
+fitdaily <- lm(H ~ A, data = HAdaily)
+summary(fitdaily)
+abline(fitdaily)
+# using daily values is better
+
+### TO DO
+identifier
+check data for certain days
 
 
-distinct_weather %>% 
-  select(site, dateTime, Tsoil0) %>% 
-  ggplot(aes(x = dateTime, y = Tsoil0)) +
-  geom_line() +
-  facet_wrap(~site)
+#### FILL MISSING VALUES IN H WITH REGRESSION
+MissingHValues <- dailyTemperature %>% 
+  filter(site %in% c("H", "A")) %>% 
+  spread(key = site, value = Tair) %>%
+  filter(is.na(H), year(dateDaily) > 2015)
+new.df <- data.frame(A = MissingHValues$A)
+newH <- predict(fit, new.df)
+
+dailyTemperature %>% 
+  mutate(Tair = ifelse(year(dateDaily) > 2015 & is.na(Tair), newH, Tair))
+  filter(site %in% c("H", "A")) %>%
+  filter(year(dateTime) > 2015, site != "L")
+
+abline(0,1, col = "red")
 
 
+### CALCULATE CUMSUM
 # Subset data for 2016, replace values below 5°C with 0, get cumsum
 CumSum2016 <- climate %>% 
   filter(year(ymd(dateDaily)) >= 2016) %>% # only 2016 data
