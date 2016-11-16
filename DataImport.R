b7e3eaaf1dfdf5fdeaf0317e922262b5538e42e5
audhalbritter <me@audhalbritter.com>
2016-11-16 10:48:48 +0100

    Move trait stuff to NewTrait

--- DataImport.R
+++ DataImport.R
@@ -93,28 +93,19 @@ pheno.long <- pheno.long %>%
   mutate(newtreat = factor(newtreat, levels=c("Control", "OTC", "Warm", "Cold"))) %>%
   mutate(pheno.stage = factor(pheno.stage, levels = c("b", "f", "s", "r", "BF", "FS", "SR")))
 
-save(pheno.long, file = "PhenoLong.RData")
 
-  # Trait data
-trait <- read_excel("SpeciesTraits2016_China.xlsx", col_names = TRUE)
-head(trait)
 
-# define flowering time
-# early: <= 4 month until June
-# mid: <= 4 month and between April and August
-# late: <= 4 month from July
-# late: >= 4 month
-trait <- trait %>% mutate(flTime = 
-                   ifelse(floweringTime %in% c("Apr-Jun", "Apr-May", "Jun", "May-Jun"), "early",
-                                 ifelse(floweringTime %in% c("Jul-Aug", "Apr-Jul", "Jul", "Jun-Jul", "May-Jul", "May-Jul-(Aug)", "summer", "Jun-Aug", "Jun-Sep"), "mid", 
-                                        ifelse(floweringTime %in% c("Aug-Nov", "Aug-Oct", "Aug-Sep", "Jul-Sep", "Jul-Oct"), "late", "always")))
-                 ) %>%
-  mutate(flTime = ifelse(sp %in% c("Car.sp.black","Car.sp.black.big","Car.sp.middle","Car.sp.yellow","Fes.sp.big","Kob.sp.sigan","Kob.sp.small","Kob.sp.yellow"), "early", flTime))
-
+# Left_join trait data
+pheno.long <- pheno.long %>% left_join(NewTrait, by = c("species" = "sp")) 
 
 # check species
-setdiff(pheno.long$species, trait$sp)
-setdiff(trait$sp, pheno.long$species)
+setdiff(pheno.long$species, NewTrait$sp)
+setdiff(NewTrait$sp, pheno.long$species)
+# These species never flowered, so not in pheno.long
+#[1] "Ana.fla" "Oxy.gla" "Cer.sze" "Cya.inf" "Gal.spa" "Hal.ell" "Hed.alg" "Pol.run" "Pri.ame" "Pru.vul" "Rho.yun" "Tri.rep" "Vio.sze"
 
-pheno.long <- pheno.long %>% left_join(trait, by = c("species" = "sp")) 
+
+# Save pheno.long
+save(pheno.long, file = "PhenoLong.RData")
+
 
