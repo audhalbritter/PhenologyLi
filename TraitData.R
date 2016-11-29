09d15f901db56202cc418cd20fdb170489bf55ba
audhalbritter <me@audhalbritter.com>
2016-11-29 09:28:21 +0100

    delete unique

--- TraitData.R
+++ TraitData.R
@@ -26,13 +26,13 @@ Flowering <- pheno.long %>%
   select(turfID, species, newtreat, value, pheno.var, pheno.stage, origSite) %>%
   filter(pheno.stage == "f") %>% 
   filter(newtreat == "Control") %>% 
-  filter(pheno.var %in% c( "first", "end", "duration")) %>%
+  filter(pheno.var %in% c( "first", "duration")) %>%
   group_by(species, pheno.var) %>% 
-  unique() %>%
+  #unique() %>%
   summarise(mean = mean(value)) %>% #mean value of each
   spread(pheno.var, mean)
 
-#obs! beacuse we do use the "origSite == M"  spoecies, so we don take care the trait of these spp,
+#obs! beacuse we do use the "origSite == M"  species, so we don take care the trait of these spp,
 #so it doesn't matter that what's the trait of M site' species
 #### early == averange first flowering time is before 182(160631)
 ## else late group
@@ -40,7 +40,7 @@ Flowering <- pheno.long %>%
 ## else general species
 NewTrait <- trait %>% left_join(Flowering, by = c("sp" = "species")) %>% #add date info to trait data
   mutate(FlTime = ifelse(first > 182, "late", "early")) %>% #before DOY:182(160631)
-  mutate(FlTime = ifelse(is.na(duration), "late", FlTime)) %>% # alreadly compared to the Warm¡¢OTC and Cold spp averange first date
+  mutate(FlTime = ifelse(is.na(duration), "late", FlTime)) %>% # alreadly compared to the Warm??OTC and Cold spp averange first date
   mutate(FlTime = ifelse(sp %in% c("Sag.jap","Kob.sp.small","Ane.dem","Car.sp.black.big", "Gen.sp.white","Pri.ame"), "early", FlTime)) %>% #Pri.ame,cut 26th,May;Ane.dem cut for floral traits at 0602
   mutate(Span = ifelse(lowerLimit > 3700, "alpine", "general")) %>%
   mutate(Span = ifelse(sp %in% c("Art.fla","Car.sp.black","Car.sp.yellow","Hal.ell","Tri.rep"),"general",
