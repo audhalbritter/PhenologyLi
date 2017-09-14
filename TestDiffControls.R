2ea5c9d9109cc1c7b36bdaa4afffe068c5bfab23
audhalbritter <me@audhalbritter.com>
2017-09-14 14:22:22 +0800

    test difference between control and local transplant

---
+++ TestDiffControls.R
@@ -0,0 +1,27 @@
+controls <- pheno.long %>%
+  left_join(meta.pheno, by = "turfID") %>% 
+  filter(treatment %in% c("C", "O"), pheno.var == "first")
+
+
+
+controls %>% 
+  select(-turfID) %>% 
+  spread(key = treatment, value = value) %>% 
+  na.omit() %>%
+  ggplot(aes(x = C, y = O)) +
+  geom_point()
+
+
+# Test if difference in phenological date between C and O
+# accounting for overdispersion
+controls$od <- as.factor(rnorm(nrow(controls)))
+fit0 <- glmer(value ~ treatment + (1|species) + (1|origSite/block) + (1|od), controls, family = "poisson")
+fit1 <- glmer(value ~ 1 + (1|species) + (1|origSite/block) + (1|od), controls, family = "poisson")
+
+ModelCheck(fit0)
+# overdispersion if p < 0.05 or overdispersion factor > 1.1
+overdisp_fun(fit0)
+anova(fit0, fit1)
+
+modsel(list(fit0, fit1), 1000)
+# no difference between the treatments!
