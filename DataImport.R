b3657a107f6b8c76ab1b07a59b6bb41673b2ff63
audhalbritter <me@audhalbritter.com>
2017-09-15 11:23:02 +0800

    Clean impossible values

--- DataImport.R
+++ DataImport.R
@@ -21,13 +21,14 @@ pheno.dat <- pheno.dat %>% filter(turfID != "")
 #head(pheno.dat)
 #str(pheno.dat) 
 
-#### META DATA ####
+
+#### CREATE META DATA ####
 meta.pheno <- pheno.dat %>% 
   distinct(turfID, origSite, destSite, block, treatment) %>% 
   mutate(newTT = plyr::mapvalues(treatment, c("1", "2", "C", "O", "OTC"), c("1", "2", "C", "C", "OTC")))
 
 
-## DATA CORRECTIONS ##
+## CORRECT SPECIES NAMES ##
 # Replace wrong names
 pheno.dat <- pheno.dat %>%
   mutate(species=replace(species,species=="Pol.leu","Pot.leu"))%>%
@@ -45,15 +46,15 @@ pheno.dat <- pheno.dat %>%
   #mutate(species = replace(species, species %in% c("Kob.sp.small", "Kob.sp.sigan", "Kob.sp.yellow"), "Kob.spp")) %>% 
   mutate(species = replace(species, species %in% c("Fes.sp.big", "Fes.sp.small"), "Fes.spp")) %>% 
   mutate(species = replace(species, species %in% c("Luz.mul"), "Luzula")) %>% 
-  mutate(species = replace(species, species %in% c("Gen.sp", "Gen.sp.white"), "Gen.spp")) %>% 
+  mutate(species = replace(species, species %in% c("Gen.sp", "Gen.sp.white"), "Gen.spp")) %>%
   mutate(species = replace(species, species %in% c("Eup.reg"), "Eup.L")) %>% 
   mutate(species = replace(species, species %in% c("Agr.sp"), "Agr.spp")) %>% 
   mutate(species = replace(species, species %in% c("Sax.lin"), "Saxifrage")) %>% 
-  mutate(species = replace(species, species %in% c("Oxy.gla"), "Oxy.yun")) %>% 
+  mutate(species = replace(species, species %in% c("Oxy.gla"), "Oxy.yun")) %>%
   mutate(species = replace(species, species %in% c("Fra.sp.2"), "Fra.spp")) %>% 
-  mutate(species = replace(species, species %in% c("Dey.pul"), "Cal.lah")) %>% 
-  mutate(species = replace(species, species %in% c("Dey.sca"), "Cal.sca")) %>% 
-  mutate(species = replace(species, species %in% c("All.cya"), "All.pra")) %>% 
+  mutate(species = replace(species, species %in% c("Dey.pul"), "Cal.lah")) %>%
+  mutate(species = replace(species, species %in% c("Dey.sca"), "Cal.sca")) %>%
+  mutate(species = replace(species, species %in% c("All.cya"), "All.pra")) %>% #???
   mutate(species = replace(species, species %in% c("Sau.cet", "Sau.gra", "Sau.hie", "Sau.pac", "Sau.sub"), "Sau.spp"))
   
 # Compare community taxa table with phenology data
@@ -61,7 +62,7 @@ setdiff(pheno.dat$species, taxa$species)
 # Ok in trait: Cya.inc, Sal.sou, Ped.ver, Sau.hie, Sau.sub, Lom.car, Bro.sin, Tar.lug
 
 
-# Calculate Sums of bud, flower etc.
+## Calculate Sums for bud, flower, seed and ripe seeds per turf
 pheno <- CalcSums(pheno.dat)
 #head(pheno)
 
@@ -90,10 +91,11 @@ pdf(file = "Phenologymaps.pdf")
 phenoMaps$pheno.maps
 dev.off()
 # **************************************************
+  
 
 
 #### CALCULATE FIRST, PEAK, END AND DURATION ####
-### MAKE LONG DATA SET ###
+## MAKE LONG DATA SET ##
 pheno.long <- pheno %>%
   select(turfID, species, date, doy, origSite, destSite, block, treatment, nr.b, nr.f, nr.s, nr.r) %>%
   gather(key = pheno.stage, value = value, nr.b, nr.f, nr.s, nr.r) %>% # make variable pheno.stage
@@ -110,9 +112,11 @@ pheno.long <- pheno %>%
   mutate(pheno.stage = substring(pheno.stage, nchar(pheno.stage), nchar(pheno.stage))) %>%  # take last letter from pheno.stage
   mutate(duration = end - (first-1)) %>% # calculate duration
   gather(key = pheno.var, value = value, -turfID, -species, -pheno.stage) %>%  # create pheno.var and gather 4 variable into 1 column
-  mutate(pheno.var = factor(pheno.var, levels = c("first", "peak", "end", "duration")))
+  mutate(pheno.var = factor(pheno.var, levels = c("first", "peak", "end", "duration"))) %>% 
+  left_join(meta.pheno, by = c("turfID"))
 
 
+#### CLEAN DATA ####
 ## List of species with more than 3 occurrences per species, site, treatment and pheno.var
 ThreeOccurences <- pheno.long %>% 
   filter(pheno.var == "first") %>% 
@@ -136,35 +140,34 @@ sp.list <- NrTreat %>%
 
 # Reduce nr. species 
 pheno.long <- pheno.long %>% 
-  left_join(meta.pheno, by = c("turfID")) %>% 
   inner_join(ThreeOccurences, by = c("species", "pheno.stage", "newTT", "origSite")) %>% 
   select(-n) %>% 
   # remove species with only one treatment
   inner_join(sp.list, by = c("species" = "Var1"))
   
-
-
-  
-
-
-########################################################################
-# Use this later!!!!
-#### CALCULATE DAYS BETWEEN FIRST BUD AND FLOWER, FLOWER AND SEED ETC (PHENO.STAGES IN DAYS) ####
+# Replace impossible values
+# Replace "first" if: b > f, f > s, s > r, b == s, f == r, b == r
+# to test use:
+# filter(pheno.var == "end", b > f) %>% arrange(species, turfID) %>% pn
 pheno.long <- pheno.long %>% 
+  mutate(pheno.stage = factor(pheno.stage, levels = c("b", "f", "s", "r"))) %>% 
   spread(key = pheno.stage, value = value) %>% 
-  # calculate difference in days between peak bud-flower and flower-seed
-  mutate(bf = ifelse(pheno.var == "peak", f-(b-1), NA), fs = ifelse(pheno.var == "peak", s-(f-1), NA), sr = ifelse(pheno.var == "peak", r-(s-1), NA)) %>%
-  gather(key = pheno.stage, value = value, b, f, s, r, bf, fs, sr) %>%
-  mutate(pheno.unit = ifelse(pheno.var == "duration", "days",
-                             ifelse(pheno.var == "peak" & pheno.stage %in% c("bf", "fs", "sr"), "days", "doy"))) %>%
-           filter(!is.na(value)) %>%  # remove empty rows
-  mutate(value = ifelse(value < 0, NA, value)) # make negative values NA
+  mutate(b = ifelse(pheno.var == "first" & b > f, NA, b)) %>% # b = NA
+  mutate(f = ifelse(pheno.var == "first" & f > s & !species %in% c("Pol.viv", "Pol.mac"), NA, f)) %>% # f = NA
+  mutate(r = ifelse(pheno.var == "first" & s > r, NA, r)) %>% # r = NA
+  mutate(b = ifelse(pheno.var == "first" & b == s, NA, b)) %>% # b = NA
+  filter(!(species == "Car.sp.yellow" & turfID == "A6-O")) %>%  # remove this turf
+  mutate(r = ifelse(species %in% c("Pol.viv", "Pol.mac"), NA, r)) %>% # remove ripe seeds for Polygonum, does not make sense, difficult to see when bulbils are ripe
+  gather(key = pheno.stage, value = value, -turfID, -species, -pheno.var, -origSite, -destSite, -block, -treatment, -newTT) %>% 
+  spread(key = pheno.var, value = value) %>% 
+  mutate(duration = ifelse(is.na(first), NA, duration)) %>%  # replace duration with NA if first is NA
+  gather(key = pheno.var, value = value, -turfID, -species, -pheno.stage, -origSite, -destSite, -block, -treatment, -newTT)
 
-########################################################################
 
 
 #### CLEAN VARIABLES ####
 phenology <- pheno.long %>%
+  mutate(year = 2016) %>% # add year
   # order sites
   mutate(destSite = factor(destSite, levels =c("H", "A", "M"))) %>% 
   mutate(origSite = factor(origSite, levels =c("H", "A", "M"))) %>% 
@@ -176,8 +179,6 @@ phenology <- pheno.long %>%
   mutate(pheno.stage = plyr::mapvalues(pheno.stage, c("b", "f", "s", "r"), c("Bud", "Flower", "Seed", "Ripe"))) %>% 
   mutate(pheno.stage = factor(pheno.stage, levels = c("Bud", "Flower", "Seed", "Ripe"))) %>% 
   mutate(pheno.unit = ifelse(pheno.var == "duration", "days", "doy"))
-  #mutate(pheno.stage = plyr::mapvalues(pheno.stage, c("b", "f", "s", "r", "bf", "fs", "sr"), c("Bud", "Flower", "Seed", "Ripe", "BudFlower", "FlowerSeed", "SeedRipe"))) %>% 
-  #mutate(pheno.stage = factor(pheno.stage, levels = c("Bud", "Flower", "Seed", "Ripe", "BudFlower", "FlowerSeed", "SeedRipe")))
 
 
 ### NEEDS TO BE FIXED!!!
@@ -194,3 +195,19 @@ setdiff(NewTrait$sp, pheno.long$species)
 save(phenology, file = "Phenology.RData")
 
 
+
+
+########################################################################
+# Maybe do not do this, data is not detailed enough to calc this !!!!
+#### CALCULATE DAYS BETWEEN FIRST BUD AND FLOWER, FLOWER AND SEED ETC (PHENO.STAGES IN DAYS) ####
+pheno.long <- pheno.long %>% 
+  spread(key = pheno.stage, value = value) %>% 
+  # calculate difference in days between peak bud-flower and flower-seed
+  mutate(bf = ifelse(pheno.var == "peak", f-(b-1), NA), fs = ifelse(pheno.var == "peak", s-(f-1), NA), sr = ifelse(pheno.var == "peak", r-(s-1), NA)) %>%
+  gather(key = pheno.stage, value = value, b, f, s, r, bf, fs, sr) %>%
+  mutate(pheno.unit = ifelse(pheno.var == "duration", "days",
+                             ifelse(pheno.var == "peak" & pheno.stage %in% c("bf", "fs", "sr"), "days", "doy"))) %>%
+  filter(!is.na(value)) %>%  # remove empty rows
+  mutate(value = ifelse(value < 0, NA, value)) # make negative values NA
+
+########################################################################
